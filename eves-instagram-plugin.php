6def29c890a25482be813d9e066a3d50750fbb0c
Eve <eve.essex@gmail.com>
2016-07-09 00:07:45 -0400

    update user id

--- eves-instagram-plugin.php
+++ eves-instagram-plugin.php
@@ -33,14 +33,12 @@ function no_ssl_http_request_args( $args, $url ) {
       // Check values
       if( $instance) {
         $title = esc_attr($instance['title']);
-        $user_id = esc_attr($instance['user_id']);
         $follow_text = esc_attr( $instance['follow_text'] );
         $textarea = esc_textarea($instance['textarea']);
         $access_key = esc_attr($instance['access_key']);
         $select = esc_attr($instance['select']);
       } else {
         $title = '';
-        $user_id = '';
         $follow_text = '';
         $textarea = '';
         $access_key = '';
@@ -51,16 +49,12 @@ function no_ssl_http_request_args( $args, $url ) {
         <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
       </p>
       <p>
-        <label for="<?php echo $this->get_field_id('access_key'); ?>"><?php _e('Instagram API Access Key:', 'instagram-feed'); ?></label>
+        <label for="<?php echo $this->get_field_id('access_key'); ?>"><?php _e('Instagram API Access Key:', 'instagram-feed'); ?></label> <small> <a href="http://jelled.com/instagram/access-token" target="_blank">What's this?</a></small>
         <input class="widefat" id="<?php echo $this->get_field_id('access_key'); ?>" name="<?php echo $this->get_field_name('access_key'); ?>" type="access_key" value="<?php echo $access_key; ?>" />
       </p>
       <p>
-        <label for="<?php echo $this->get_field_id('user_id'); ?>"><?php _e('User ID:', 'instagram-feed'); ?> </label>
-        <input class="widefat" id="<?php echo $this->get_field_id('user_id'); ?>" name="<?php echo $this->get_field_name('user_id'); ?>" type="user_id" value="<?php echo $user_id; ?>" />
-      </p>
-      <p>
         <input id="<?php echo esc_attr( $this->get_field_id( 'follow_text' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'follow_text' ) ); ?>" type="checkbox" value="1" <?php checked( '1', $follow_text ); ?> />
-        <label for="<?php echo esc_attr( $this->get_field_id( 'follow_text' ) ); ?>"><?php _e( 'Display follow text & link to this account.', 'instagram-feed' ); ?></label>
+        <label for="<?php echo esc_attr( $this->get_field_id( 'follow_text' ) ); ?>"><?php _e( 'Display link to this account.', 'instagram-feed' ); ?></label>
       </p>
 
       <p>
@@ -87,7 +81,6 @@ function no_ssl_http_request_args( $args, $url ) {
       // Fields
       $instance['title'] = strip_tags($new_instance['title']);
       $instance['access_key'] = strip_tags($new_instance['access_key']);
-      $instance['user_id'] = strip_tags($new_instance['user_id']);
       $instance['follow_text'] = strip_tags($new_instance['follow_text']);
       $instance['textarea'] = strip_tags($new_instance['textarea']);
       $instance['select'] = strip_tags($new_instance['select']);
@@ -100,15 +93,13 @@ function no_ssl_http_request_args( $args, $url ) {
       // Fetch widget params
       $title = apply_filters('widget_title', $instance['title']);
       $access_key = $instance['access_key'];
-      $user_id = $instance['user_id'];
       $follow_text = $instance['follow_text'];
       $textarea = $instance['textarea'];
       $select = $instance['select'];
       echo $before_widget;
 
       // Get remote data
-// $request_url = 'https://www.instagram.com/newmusicusa/media/'
-      $request_url = 'https://api.instagram.com/v1/users/'.$user_id.'/media/recent?count=6&access_token=' . $instance['access_key'];
+      $request_url = 'https://api.instagram.com/v1/users/self/media/recent?count=6&access_token=' . $instance['access_key'];
       $result = wp_remote_get( $request_url );
       extract( $args );
 
